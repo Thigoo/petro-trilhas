42ba0895c74b7af20b43f3587eb22de13ead3f52
Thigoo <thiagomvk08@gmail.com>
2026-03-31 18:53:55 -0300

    feat: organiza layout

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/src/components/shared/Header";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      {/* O Header fica aqui para aparecer em todas as rotas deste grupo */}
+      <Header />
+
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
